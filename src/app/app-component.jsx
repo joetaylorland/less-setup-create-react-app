e88405f70a4a7d137e046b33938a8ab3c5060971
Joe Taylor <joetaylorland@users.noreply.github.com>
2018-09-26 10:42:48 +0100

    corrected faulty bems class

--- src/app/app-component.jsx
+++ src/app/app-component.jsx
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-          <h1 className="app_text app__text--title">Welcome to React</h1>
+          <h1 className="app__text app__text--title">Welcome to React</h1>
         <p className="app__text">
           This simple app shows LESS auto-watching and compiling.
         </p>
