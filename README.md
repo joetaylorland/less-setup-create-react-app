51c3f6222aec6808802291812d7260e5c9a74015
Joe Taylor <joetaylorland@users.noreply.github.com>
2018-09-26 10:33:56 +0100

    first push

---
+++ README.md
@@ -0,0 +1,5 @@
+# Using LESS with React
+
+Run ```npm install``` then ```npm start```.
+
+The sample app will run at localhost:4400. The folder styles/styles.less is the master LESS file that is compiled to the necessary css file for index.js. Import your component LESS files here and everything will be auto-compiled as you make changes.
---
+++ src/app/app-component.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+          <h1 className="app_text app__text--title">Welcome to React</h1>
+        <p className="app__text">
+          This simple app shows LESS auto-watching and compiling.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
